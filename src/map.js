e6d82c5e42304dc24ae6919c79f3da8d487c916b
gstfnk <gabrysia.stefaniak@gmail.com>
2022-07-21 16:13:04 +0200

    learn about destructuring

--- src/map.js
+++ src/map.js
@@ -1,3 +1,6 @@
-export const map = function () {
-
-};
+export const map = ({ avatar_url: img = '', bio = 'Cannot get a biography.' } = {}) => ({
+    // return { img: avatar_url, bio: bio };
+    // return { img: avatar_url, bio };
+        img,
+        bio
+});
--- src/map.spec.js
+++ src/map.spec.js
@@ -4,18 +4,20 @@ const {map} = require("./map"); // assert from Node
 describe('map', () => {
     it('should convert input to object with different keys', () => {
         // given
-        const givenUrl = 'url';
-        const givenBio = 'bio';
+        // const givenUrl = 'url';
+        // const givenBio = 'bio';
+        // destructuring
+        const [givenUrl, givenBio] = ['url', 'bio'];
         const input = {
-            img: givenUrl,
+            avatar_url: givenUrl,
             bio: givenBio
         };
 
         // when
         // it won't work, because map func return undefined
-        const result = map(input)
+        const result = map(input);
 
         // then
-        assert.deepEqual(result, {img: givenUrl, bio: givenBio});
+        assert.deepEqual(result, { img: givenUrl, bio: givenBio });
     })
 });
