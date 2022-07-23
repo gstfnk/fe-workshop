f2718279de56c2a86088cbbec64547333df18df3
gstfnk <gabrysia.stefaniak@gmail.com>
2022-07-24 00:15:00 +0200

    cleaning

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-const user = require('./user');
+import initAboutMe from './about-me/index';
 
-console.log(user.avatar_url);
+initAboutMe();
--- src/map.js
+++
@@ -1,6 +0,0 @@
-export const map = ({ avatar_url: img = '', bio = 'Cannot get a biography.' } = {}) => ({
-    // return { img: avatar_url, bio: bio };
-    // return { img: avatar_url, bio };
-        img,
-        bio
-});
