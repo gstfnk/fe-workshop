5c51bde61031e93eea235485dfaf4cc6853e2d72
gstfnk <gabrysia.stefaniak@gmail.com>
2022-07-28 19:30:36 +0200

    rewriting to react

--- src/github/react-component.tsx
+++ src/github/react-component.tsx
@@ -1,5 +1,35 @@
 import * as React from 'react';
 import {GitHubRepo} from "./model";
+import {getRepos} from "./service";
+
+interface IReposProps {
+    user: string;
+}
+
+export class Repos extends React.Component<IReposProps, {repos: GitHubRepo[]}>{
+    constructor(props: IReposProps) {
+        super(props);
+        this.state = {repos: []};
+    }
+
+    async componentDidMount() {
+        this.setState({repos: await getRepos(this.props.user)})
+    }
+
+    render() {
+        return (<RepoTable repos={this.state.repos}/>);
+    }
+}
+
+function RepoTable({repos}: { repos: GitHubRepo[] }) {
+    return (
+        <table className="pure-table pure-table-bordered" style={{margin: '0 auto'}}>
+            <tbody>
+            {repos.map(r => <RepoTableRow {...r} key={r.name}/>)}
+            </tbody>
+        </table>
+    );
+}
 
 export function RepoTableRow({name, stars, url}: GitHubRepo) {
     return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,23 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import initAboutMe from './about-me';
+import {Repos} from "./github/react-component";
 
 initAboutMe();
 
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            'about-me': { me: string }
+        }
+    }
+}
+
+const USER = 'gstfnk';
 ReactDOM.render(
-  <h1>Hello, react-world!</h1>,
-  document.getElementById('repos')
+    <>
+        <about-me me={USER}/>
+        <Repos user={USER}/>
+    </>,
+    document.getElementById('app')
 );
